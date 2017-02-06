ad65cf08128342a752996f515aac50aa67227ed4
Ammy Pandey <ammypandey@Ammys-MacBook-Air.local>
2017-02-06 13:45:56 +0530

    Initial Commit

---
+++ Roll_Dice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Roll_Dice
+//
+//  Created by Ammy Pandey on 06/02/17.
+//  Copyright © 2017 Ammy Pandey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
